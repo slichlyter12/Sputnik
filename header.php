f45a7a10f091cad74879f57ccd06f2b923e99793
schneidz1636 <schneidz@oregonstate.edu>
2015-11-24 15:37:43 -0800

    Added signup button

--- header.php
+++ header.php
@@ -18,14 +18,13 @@
       </ul>
 	  <form class="navbar-form navbar-right" role="form">
 		<div class="form-group">
-		  <label for="email">Email:</label>
 		  <input type="email" class="form-control" id="email" placeholder="Enter email">
 		</div>
 		<div class="form-group">
-		  <label for="pwd">Password:</label>
 		  <input type="password" class="form-control" id="pwd" placeholder="Enter password">
 		</div>
-		<button type="submit" class="btn btn-default">Login</button>
+		<button type="submit" class="btn btn-default">Login</button><!--White button-->
+		<button type="submit" class="btn btn-primary">Sign Up</button><!--Blue button-->
 	  </form>
 	</div>
   </div>
