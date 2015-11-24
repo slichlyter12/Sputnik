f4a553b6ae73cd0689aa3fae73c65ff2e25fc846
schneidz1636 <schneidz@oregonstate.edu>
2015-11-24 15:13:13 -0800

    Renamed donate.php, added header tag

--- donate.php
+++ donate.php
@@ -24,36 +24,7 @@
 
 <body>
 
-<!-- Header end -->
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Donate Central</a>
-    </div>
-    <div>
-      <ul class="nav navbar-nav">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="#">View Companies</a></li>
-        <li><a href="#">Track Donations</a></li>
-		<li class="active"><a href="donate.html">Donate Now</a></li>        <!-- Allow guest to donate? -->
-      </ul>
-    </div>
-	<div style ="float: right; margin-top:7px;">
-		<form class="form-inline" role="form">
-		<div class="form-group">
-		  <label for="email">Email:</label>
-		  <input type="email" class="form-control" id="email" placeholder="Enter email">
-		</div>
-		<div class="form-group">
-		  <label for="pwd">Password:</label>
-		  <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-		</div>
-		<button type="submit" class="btn btn-default">Login</button>
-	  </form>
-	</div>
-  </div>
-</nav>
-<!-- Header end -->
+<?php include_once "header.php"; ?>
 
 <div class="container">
   <h1 style="text-align: center; margin-top: 50px">Donate Now</h1>
