25ceb976fdb6cf7ab4936ec625a82385202e8e0a
schneidz1636 <schneidz@oregonstate.edu>
2015-11-24 14:44:38 -0800

    Added header.php

---
+++ header.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+	<div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	  <a class="navbar-brand" href="#">Donate Central</a>
+    </div>
+  
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="#">View Companies</a></li>
+        <li><a href="#">Track Donations</a></li>
+		<li><a href="donate.html">Donate Now</a></li>
+      </ul>
+	  <form class="navbar-form navbar-right" role="form">
+		<div class="form-group">
+		  <label for="email">Email:</label>
+		  <input type="email" class="form-control" id="email" placeholder="Enter email">
+		</div>
+		<div class="form-group">
+		  <label for="pwd">Password:</label>
+		  <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+		</div>
+		<button type="submit" class="btn btn-default">Login</button>
+	  </form>
+	</div>
+  </div>
+</nav>
