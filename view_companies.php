6b01e808d72727c9f119063c86a794eef10d558c
Sam Lichlyter <lichlyts@onid.oregonstate.edu>
2015-12-03 13:29:35 -0800

    added sorting functionality to view companies

--- view_companies.php
+++ view_companies.php
@@ -20,11 +20,41 @@
 		
 		<?php include_once 'header.php'; ?>
 		<div class="container">
-			<div class="page-header"><h1>Charity Profiles</h1></div>
+			<div class="page-header">
+				<h1>Charity Profiles</h1>
+				<small>Sort By&hellip;</small>
+				<div class="btn-group">
+					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" ari-haspopup="true" aria-expanded="false">Charity Type <span class="caret"></span></button>
+					<ul class="dropdown-menu" id="type">
+						<li><a href="?type=Medical">Medical</a></li>
+						<li><a href="?type=Food/Water">Food / Water</a></li>
+						<li><a href="?type=Service">Service</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="view_companies.php">All</a></li>
+					</ul>
+				</div>
+				<div class="btn-group">
+					<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" ari-haspopup="true" aria-expanded="false">Desired Donation Type <span class="caret"></span></button>
+					<ul class="dropdown-menu" id="donation_type">
+						<li><a href="?donation_type=Money">Money</a></li>
+						<li><a href="?donation_type=Canned Food">Canned Food</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="view_companies.php">All</a></li>
+					</ul>
+				</div>
+			</div>
 			<div class="row">
 				<?php
 					
-					$query = "SELECT cid, name, total_received, img FROM charity";
+					if (isset($_GET["type"]) && !empty($_GET["type"])) {
+						$type = mysqli_real_escape_string($mysqli, strip_tags($_GET["type"]));
+						$query = "SELECT cid, name, total_received, img FROM charity WHERE type='$type'";
+					} else if (isset($_GET["donation_type"]) && !empty($_GET["donation_type"])) {
+						$donation_type = mysqli_real_escape_string($mysqli, strip_tags($_GET["donation_type"]));
+						$query = "SELECT cid, name, total_received, img FROM charity WHERE desired_aid='$donation_type'";
+					} else {
+						$query = "SELECT cid, name, total_received, img FROM charity";
+					}
 					if ($result = mysqli_query($mysqli, $query)) {
 						while ($row = mysqli_fetch_row($result)) {
 							echo "
